1aab08ef0fabf1eea53143dc1b4623bb88f30ed6
wiseman88 <wiseadam02@gmail.com>
2023-11-13 19:44:19 +0100

    feat(category): add filament category
    Add resources for Category using filament. Generated
    files within CategoryResource folder are responsible for
    CRUD operations of Category object. CategoryResource.php
    is responsible for generation of UI of categories view
    and CRUD page view.

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Group::make()
+                    ->schema([
+                        Forms\Components\Section::make([
+                            Forms\Components\TextInput::make('name')
+                                ->required()
+                                ->unique()
+                                ->live(onBlur: true)
+                                ->afterStateUpdated(function ($state, Forms\Set $set) {
+                                    $set('slug', Str::slug($state));
+                                }),
+                            Forms\Components\TextInput::make('slug')
+                                ->disabled()
+                                ->dehydrated()
+                                ->required()
+                                ->unique(Category::class, 'slug', ignoreRecord: true),
+                            Forms\Components\Select::make('parent_id')
+                                ->label('Parent category')
+                                ->relationship('parent', 'name'),
+                            Forms\Components\MarkdownEditor::make('description')
+                                ->columnSpanFull(),
+                            Forms\Components\Toggle::make('visible')
+                                ->helperText('Enable or disable category visibility')
+                                ->default(false)
+                        ]),
+                    ]),
+                Forms\Components\Section::make('Image')
+                    ->schema([
+                        Forms\Components\FileUpload::make('image')
+                            ->directory('form-attachments')
+                            ->preserveFilenames()
+                            ->image()
+                            ->imageEditor(),
+                    ])->collapsible(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug'),
+                Tables\Columns\TextColumn::make('description'),
+                Tables\Columns\ToggleColumn::make('visible'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
