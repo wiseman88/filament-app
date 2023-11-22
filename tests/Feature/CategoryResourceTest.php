f854a57290a0bb8b0ea9817da615654d4091197a
wiseman88 <wiseadam02@gmail.com>
2023-11-22 18:43:48 +0100

    test(category): add tests for category
    Add tests for category resource to check that
    create page can render and listing of created
    categories working. Add category factory to seed
    database with fake data.

--- tests/Feature/CategoryResourceTest.php
+++ tests/Feature/CategoryResourceTest.php
@@ -3,13 +3,25 @@
 namespace Tests\Feature;
 
 use App\Filament\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
+use function Pest\Livewire\livewire;
 use function Pest\Laravel\get;
+use Illuminate\Support\Str;
 
 it('can render page', function () {
     get(CategoryResource::getUrl('index'))->assertOk();
 });
 
-it('it renders the index page', function () {
-    expect(true)->toBe(true);
+it('can render create page', function () {
+    get(CategoryResource::getUrl('create'))->assertOk();
+});
+
+it('can list category list', function () {
+    $categories = Category::factory()->count(10)->create();
+
+    livewire(CategoryResource\Pages\ListCategories::class)
+        ->assertCanSeeTableRecords($categories);
 });
