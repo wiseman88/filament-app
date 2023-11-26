fea69e3ddef8d9e0fdd124dc8f82c8af69ad312b
wiseman88 <wiseadam02@gmail.com>
2023-11-26 17:26:15 +0100

    test(category): add tests for edit
    Add tests for edit function of filament app.

--- tests/Feature/CategoryResourceTest.php
+++ tests/Feature/CategoryResourceTest.php
@@ -25,3 +25,61 @@ it('can list category list', function () {
     livewire(CategoryResource\Pages\ListCategories::class)
         ->assertCanSeeTableRecords($categories);
 });
+
+it('can render edit page', function () {
+    get(CategoryResource::getUrl('edit', [
+        'record' => Category::factory()->create(),
+    ]))->assertOk();
+});
+
+it('can retrieve data', function () {
+    $category = Category::factory()->create();
+    $name = $category->name;
+
+    livewire(CategoryResource\Pages\EditCategory::class, [
+        'record' => $category->getRouteKey(),
+    ])
+        ->assertFormSet([
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'description' => $category->description,
+            'visible' => $category->visible,
+        ]);
+});
+
+it('can save edited data', function () {
+    $category = Category::factory()->create();
+    $newData = Category::factory()->make();
+    $name = $newData->name;
+
+    livewire(CategoryResource\Pages\EditCategory::class, [
+        'record' => $category->getRouteKey(),
+    ])
+        ->fillForm([
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'description' => $newData->description,
+            'visible' => $newData->visible,
+        ])
+        ->call('save')
+        ->assertHasNoFormErrors();
+
+    expect($category->refresh())
+        ->name->toBe($newData->name)
+        ->slug->toBe(Str::slug($newData->name))
+        ->description->toBe($newData->description)
+        ->visible->toBe((int)$newData->visible);
+});
+
+it('can validate edit input', function () {
+    $category = Category::factory()->create();
+
+    livewire(CategoryResource\Pages\EditCategory::class, [
+        'record' => $category->getRouteKey(),
+    ])
+        ->fillForm([
+            'name' => null,
+        ])
+        ->call('save')
+        ->assertHasFormErrors(['name' => 'required']);
+});
