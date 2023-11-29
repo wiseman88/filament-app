dea50ec0fc7dd7067f6c97e88607462a81b27ba9
wiseman88 <wiseadam02@gmail.com>
2023-11-29 15:25:33 +0100

    feat(product): add model and migration
    - Create Product model with necessary attributes.
    - Add migration file for Products table with schema definition.

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'category_id',
+        'slug',
+        'sku',
+        'description',
+        'quantity',
+        'price',
+        'visible',
+        'image'
+    ];
+}
---
+++ database/migrations/2023_11_29_135652_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            $table->string('slug')->unique();
+            $table->string('sku')->unique();
+            $table->string('image');
+            $table->longText('description')->nullable();
+            $table->unsignedBigInteger('quantity');
+            $table->decimal('price', 10, 2);
+            $table->boolean('visible')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
