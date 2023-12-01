eac85d11bf411c21512ed4f47f177ec227df8d3a
wiseman88 <wiseadam02@gmail.com>
2023-12-01 18:06:32 +0100

    test(product): add tests for product resource

--- tests/Feature/ProductResourceTest.php
+++ tests/Feature/ProductResourceTest.php
@@ -1,5 +1,19 @@
 <?php
 
-it('', function () {
+use App\Filament\Resources\ProductResource;
+use App\Models\Product;
+use function Pest\Laravel\get;
 
+it('can render index page', function () {
+    get(ProductResource::getUrl('index'))->assertOk();
+});
+
+it('can render create page', function () {
+    get(ProductResource::getUrl('create'))->assertOk();
+});
+
+it('can render edit page', function () {
+    get(ProductResource::getUrl('edit', [
+        'record' => Product::factory()->create(),
+    ]))->assertOk();
 });
