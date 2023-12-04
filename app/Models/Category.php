fac9fb6844d9e0951c8da4a82c2c02637928d549
wiseman88 <wiseadam02@gmail.com>
2023-12-04 12:30:33 +0100

    feat(models): add relations
    Add relations belongsToMany for product and
    category model to retrieve all products of
    category and all categories for selected
    product.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
@@ -29,4 +30,10 @@ class Category extends Model
     {
         return $this->hasMany(Category::class, 'parent_id');
     }
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
 }
