2ea4b045264a6c7f34a0852459a1de6216ee2baf
wiseman88 <wiseadam02@gmail.com>
2023-12-08 13:09:11 +0100

    Revert "feat(product): add for content"
    This reverts commit 17f3173b5878f0b52b27ccf52d0f986c3ef5c759.

--- app/Filament/Resources/ProductResource/Pages/CreateProduct.php
+++ app/Filament/Resources/ProductResource/Pages/CreateProduct.php
@@ -3,23 +3,10 @@
 namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
-use App\Models\Product;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
-use Illuminate\Database\Eloquent\Model;
 
 class CreateProduct extends CreateRecord
 {
     protected static string $resource = ProductResource::class;
-
-    protected function handleRecordCreation(array $data): Model
-    {
-        $categoryId = $data['category_id'];
-        unset($data['category_id']);
-
-        $product = Product::create($data);
-        $product->categories()->sync($categoryId);
-
-        return $product;
-    }
 }
--- app/Filament/Resources/ProductResource/Pages/EditProduct.php
+++ app/Filament/Resources/ProductResource/Pages/EditProduct.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
-use Illuminate\Database\Eloquent\Model;
 
 class EditProduct extends EditRecord
 {
@@ -16,16 +15,5 @@ class EditProduct extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
-    }
-
-    protected function handleRecordUpdate(Model $record, array $data): Model
-    {
-        $categoryId = $data['category_id'];
-        unset($data['category_id']);
-
-        $record->update($data);
-        $record->categories()->sync($categoryId);
-
-        return $record;
     }
 }
