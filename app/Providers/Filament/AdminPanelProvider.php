5bb3073158d3eb0417efbfde9f54a90865ba8841
wiseman88 <wiseadam02@gmail.com>
2023-12-10 14:55:10 +0100

    chore(app): change route and id
    Change route of app to '/' instead of admin
    and change id application from admin to app

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,8 +24,8 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
-            ->id('admin')
-            ->path('admin')
+            ->id('app')
+            ->path('/')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
